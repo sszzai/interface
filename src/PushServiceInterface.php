78a9359f7ea39b799ee731de925e7ed09840b189
Havo <358223207@qq.com>
2021-11-19 10:49:11 +0800

    添加推送聊天消息接口

---
+++ src/PushServiceInterface.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @desc 推送消息服务
+ * @author havo
+ * @date 2021/11/19 10:46
+ * @copyright 小码科技
+ */
+
+namespace Sszzai\MicroService;
+
+class PushServiceInterface
+{
+
+
+    /**
+     * 推送聊天消息
+     * @param $msg
+     */
+    public function pushChatMsg($msg){
+
+    }
+
+}
