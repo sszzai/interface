9b79a02718d7902839d07d9684d6cb356659e0a5
Havo <358223207@qq.com>
2021-11-22 17:19:17 +0800

    添加推送注册和解绑微服务

--- src/PushServiceInterface.php
+++ src/PushServiceInterface.php
@@ -11,6 +11,32 @@ namespace Sszzai\MicroService;
 interface PushServiceInterface
 {
 
+    /**
+     * 推送离线聊天消息
+     * @param $msg
+     * @return mixed
+     */
     public function pushChatMsg($msg);
+
+
+    /**
+     * 推送设备注册
+     * @param $type 推送平台类型
+     * @param $register_id 注册ID
+     * @param int $user_id 用户ID
+     * @param array params 其他参数
+     */
+    public function register($type,$register_id,$user_id=0,$params=[]);
+
+
+    /**
+     * 设备与用户解绑
+     * @param $type 推送平台
+     * @param $register_id 注册ID
+     * @param $user_id 用户ID
+     * @return mixed
+     */
+    public function unbind($type,$register_id,$user_id);
+
 
 }
